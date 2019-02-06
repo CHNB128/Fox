19ff7ec2e79cf695dffc257b722326ef771b2fe6
drake.aren <drake.aren@protonmail.com>
2019-02-06 09:46:30 +0600

    initial commit

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule Fox.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :fox,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger,  :httpoison]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:floki, "~> 0.20.0"},
+      {:httpoison, "~> 1.4"},
+      {:json, "~> 1.2"},
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+end
